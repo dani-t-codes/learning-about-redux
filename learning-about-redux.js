53e13e38cbbf0da4e49b676bb2cce6eb216ca742
Danielle Thompson <danithompson74@gmail.com>
2021-02-14 21:37:57 -0800

    Add notes to code for later instruction

--- learning-about-redux.js
+++ learning-about-redux.js
@@ -19,14 +19,25 @@ const ticketListReducer = (state = {}, action) => {
   }
 };
 
+//start with destructuring to get createStore fxn from Redux in CDN
 const { createStore } = Redux;
 
+//create the store which holds app's state tree
+//state tree is immutable and is replaced with a new immutable struc on each update
+//createStore takes a reducer as an argument - reducer tells store how to handle actions
 const store = createStore(ticketListReducer);
 
+//checks the state of store with getState() method being called *on* store
+//logs initial state
 console.log(store.getState());
 
+// in order to actually log changes to state, need to listen for changes with subscribe() method
+// re: pubsub pattern - store acts as intermediary btw sub(scribing) and pub(lishing)
+// naming it in constant unsubscribe allows for unsubscribing later
 const unsubscribe = store.subscribe(() => console.log(store.getState()));
 
+//dispatch() method publishes
+//each time an action is dispatched, it will trigger subscription and log the result to store.getState()
 store.dispatch({
   type: 'ADD_TICKET',
   names: 'Jasmine and Justine',
@@ -47,4 +58,6 @@ store.dispatch({
   id: 1
 });
 
+//ends the subscription
 unsubscribe();
+//when bindings combine React Redux, will handle subscribe under the hood
